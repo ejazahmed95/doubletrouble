f117f8f8cdf4c922fb64dae3fe9eb69d70e2a057
root <root@eae-z490-046.ad.utah.edu>
2022-09-15 17:58:43 -0600

    Added TMPro; input system movement

---
+++ Assets/DoubleTrouble/Scripts/Movement.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace DoubleTrouble.Scripts {
+	public class Movement : MonoBehaviour {
+		[SerializeField] private Rigidbody2D _body;
+
+		[SerializeField] private InputActionAsset controls;
+		private InputActionMap _inputActionMap;
+
+		private InputAction _moveAction;
+
+		private void Start() {
+			// _inputActionMap = controls.FindActionMap("Player");
+			// _moveAction = _inputActionMap.FindAction("Move");
+			// _moveAction.performed += OnMove;
+		}
+
+		private void OnMove(InputAction.CallbackContext ctx) {
+			ctx.ReadValue<Vector2>();
+		}
+	}
+}
